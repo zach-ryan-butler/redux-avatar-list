69ae2d979cde911fba14b313d5d76fbff5db601f
Zach Butler <zach.standley93@gmail.com>
2019-08-30 15:14:13 -0700

    removed console log

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -2,16 +2,6 @@ import { getCharacters } from '../services/avatarApi';
 
 export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
 export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
-// export const fetchCharacters = () => dispatch => {
-//   dispatch({ type: FETCH_CHARACTERS_LOADING });
-//   getCharacters()
-//     .then(characters => {
-//       dispatch({
-//         type: FETCH_CHARACTERS,
-//         payload: characters
-//       });
-//     });
-// };
 
 export const fetchCharacters = () => ({
   type: FETCH_CHARACTERS,
