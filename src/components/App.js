3ab89aa6910c06f6773c05f299e0edf49789810a
Zach Butler <zach.standley93@gmail.com>
2019-08-30 15:14:28 -0700

    implemented Router, Switch, and Route to my app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import AllCharacters from '../container/AllCharacters';
+import AllCharacterDetail from '../container/AllCharacterDetail';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default function App() {
-  return <AllCharacters />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/:name/:id" component={AllCharacterDetail}/>
+        <Route path="/" component={AllCharacters}/>
+      </Switch>
+    </Router>
+  );
 }
  
