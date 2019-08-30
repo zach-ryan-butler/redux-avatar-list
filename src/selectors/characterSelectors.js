c8be42e9b8c5e0e36c81699f9ad6b967e5d9e1f6
Zach Butler <zach.standley93@gmail.com>
2019-08-30 15:15:22 -0700

    finds a character from state by matching the id from the url to a characters id

--- src/selectors/characterSelectors.js
+++ src/selectors/characterSelectors.js
@@ -1,2 +1,3 @@
 export const getCharacters = state => state.characters.characters;
 export const getCharactersLoading = state => state.characters.loading;
+export const getCharacter = (state, id) => state.characters.characters.find(c => c._id === id);
